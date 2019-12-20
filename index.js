e1758e95b6ce89e6d6101094e452b5aa1a2d4a87
arraaashu <arra.aashritha@gmail.com>
2019-12-20 14:56:24 +0530

    adding project files

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('frontend'))
+
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+var student={"name":"being zero","college":"vnr","regno":1205}
+app.get('/data',(req,res) => res.json(student));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
